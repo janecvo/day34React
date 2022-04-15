59dca25369bf5498fb6e85deb60d8f6f3e5e1458
janecvo <janecvo@gmail.com>
2022-04-15 08:24:06 -0400

    fix

--- src/content.jsx
+++ src/content.jsx
@@ -5,10 +5,9 @@ import Part from './part';
 const Content = ({parts}) => {
     return (
         <div>
-        {/* returns name of parts of course; array elements */}
-        <Part part={}={parts[0]}/>  
-        <Part part={}={parts[1]}/>
-        <Part part={}={parts[2]}/>
+        <Part part={parts[0]}/>  
+        <Part part={parts[1]}/>
+        <Part part={parts[2]}/>
         </div>
     );
 };
